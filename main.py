bc3d79b6993cbbe8b415b99be2276079b3037e06
John Exim <jonh.exim.ece@gmail.com>
2020-10-28 02:07:18 +0800

    Add pay processors and update main

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
+import uvicorn
 from fastapi import FastAPI
 from base_models import PaymentInfo
+from pay_processors import PayProcessors
 
 app = FastAPI()
 
 @app.post("/payments/")
 async def process_payments(payment_info: PaymentInfo):
 
-    result = []
-    
-    return result
+    if payment_info.amount <= 20:
+        return PayProcessors.process_cheap()
+    elif payment_info.amount <= 500:
+        return PayProcessors.process_expensive()
+    else:
+        return PayProcessors.process_premium()
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
---
+++ pay_processors.py
@@ -0,0 +1,41 @@
+import random
+
+class PayProcessors():
+
+    SUCCESS_MESSAGE = {"message" : "Your payment was successfully processed."}
+    FAIL_MESSAGE = {"message" : "Your payment was NOT processed."}
+
+    @classmethod
+    def process_cheap(cls):
+        if is_cheap_gateway_available():
+            return cls.SUCCESS_MESSAGE
+        else:
+            return cls.FAIL_MESSAGE
+
+    @classmethod
+    def process_expensive(cls):
+        if is_expensive_gateway_available():
+            return SUCCESS_MESSAGE
+        else:
+            cls.process_cheap()
+
+    @classmethod
+    def process_premium(cls):
+        attempts = 0
+        while attempts <= 3:
+            if is_premium_gateway_available():
+                return cls.SUCCESS_MESSAGE
+            else:
+                attempts += 1
+
+        return cls.FAIL_MESSAGE
+
+
+def is_cheap_gateway_available():
+    return random.random() > 0.5
+
+def is_expensive_gateway_available():
+    return random.random() > 0.3
+
+def is_premium_gateway_available():
+    return random.random() > 0.2
